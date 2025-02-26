c260e0797a90296e0580e0ca86d051179a83fa57
keshew <artyom.korotckov2017@yandex.ru>
2025-02-27 00:39:19 +0500

    Initial Commit

---
+++ SweetBonanza/SweetBonanzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetBonanzaApp.swift
+//  SweetBonanza
+//
+//  Created by Артём Коротков on 27.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct SweetBonanzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
